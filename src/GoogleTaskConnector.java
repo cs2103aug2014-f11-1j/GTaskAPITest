cb78b8e1288e9fc4df8a174965df20b4ae95566d
Michelle Tan <michelletanjm@gmail.com>
2014-09-22 13:38:12 +0800

    Create Basic Project Files
    -Add GoogleTaskConnector class
    -Add GTaskAPITest class

---
+++ src/GoogleTaskConnector.java
@@ -0,0 +1,15 @@
+
+/**
+ * This class is used to connect to Google Task API.
+ * 
+ * To use this class, the user has to provide the
+ * details of their Google account and sign in.
+ * This class can create, read, update or delete tasks
+ * for the given Google account.
+ * 
+ * @author Michelle Tan
+ */
+public class GoogleTaskConnector {
+	
+
+}
