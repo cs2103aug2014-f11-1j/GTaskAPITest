99ab0d4fa1fc67cba33ac8c675481afa31bce9ad
Michelle Tan <michelletanjm@gmail.com>
2014-09-23 23:16:43 +0800

    Fix IllegalArgumentException Error In Method setUp
    -Add saving credentials in file for offline access
    -Add request for request token for offline access
    -Extract methods to apply SLAP
    -Shift frequently used variables to class-level
    -Add variables to allow toggling of options in authorisation code flow

--- src/GoogleTaskConnector.java
+++ src/GoogleTaskConnector.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -12,6 +13,7 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.tasks.Tasks;
 import com.google.api.services.tasks.TasksScopes;
 import com.google.api.services.tasks.model.Task;
@@ -34,10 +36,20 @@ public class GoogleTaskConnector {
 
 	private static final String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 
+	private static final String DATA_STORE_FILE_NAME = "C:/Users/Mishie/Documents/Eclipse_Workspaces/CS2103/GTaskAPITest/credentials";
+
 	private static final String MESSAGE_EXCEPTION_IO = "Unable to read the data retrieved.";
 	private static final String MESSAGE_ARGUMENTS_NULL = "Null arguments given.";
+	
+	// Option to request access type for application. Can be "online" or "offline".
+	private static final String FLOW_ACCESS_TYPE = "offline";
+	// Option to request approval prompt type for application. Can be "force" or "auto".
+	private static final String FLOW_APPROVAL_PROMPT = "auto";
 
 	private Tasks service;
+	private HttpTransport httpTransport;
+	private JsonFactory jsonFactory;
+	private GoogleAuthorizationCodeFlow flow;
 
 	/**
 	 * Returns a GoogleTaskConnector after trying to 
@@ -45,6 +57,8 @@ public class GoogleTaskConnector {
 	 * 
 	 */
 	public GoogleTaskConnector() {
+		httpTransport = new NetHttpTransport();
+		jsonFactory = new JacksonFactory();
 		setUp();
 	}
 
@@ -58,38 +72,109 @@ public class GoogleTaskConnector {
 	 * 
 	 */
 	public void setUp(){
-		HttpTransport httpTransport = new NetHttpTransport();
-		JsonFactory jsonFactory = new JacksonFactory();
 
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+		File dataStoreFile = new File(DATA_STORE_FILE_NAME);
+
+		try {
+			FileDataStoreFactory fdsf = new FileDataStoreFactory(dataStoreFile);
+			flow = buildAuthorisationCodeFlow(httpTransport, jsonFactory, fdsf);
+		} catch (IOException e) {
+			System.out.println(MESSAGE_EXCEPTION_IO);
+		}
+
+		askUserForAuthorisationCode(flow);
+		String code = getUserInput();
+
+		GoogleTokenResponse response = getTokenResponse(flow, code);
+		GoogleCredential credential = buildCredential(response);
+		service = new Tasks.Builder(httpTransport, jsonFactory, credential)
+		.setApplicationName(APPLICATION_NAME).build();
+
+	}
+
+	/**
+	 * Builds a GoogleCredential for use in Google API requests.
+	 * @param response
+	 * @return           Credential
+	 */
+	private GoogleCredential buildCredential(GoogleTokenResponse response) {
+		GoogleCredential credential = new GoogleCredential.Builder()
+		.setJsonFactory(jsonFactory)
+		.setTransport(httpTransport)
+		.setClientSecrets(CLIENT_ID, CLIENT_SECRET).build()
+		.setAccessToken(response.getAccessToken())
+		.setRefreshToken(response.getRefreshToken());
+		return credential;
+	}
+
+	/**
+	 * Sends a token request to get a GoogleTokenRequest.
+	 * If an IOException occurs, returns null.
+	 * 
+	 * @param flow
+	 * @param code
+	 * @return      Token response
+	 */
+	private GoogleTokenResponse getTokenResponse(GoogleAuthorizationCodeFlow flow, String code) {
+		try {
+			GoogleTokenResponse response = flow.newTokenRequest(code)
+					.setRedirectUri(REDIRECT_URI).execute();
+			return response;
+		} catch (IOException e) {
+			System.out.println(MESSAGE_EXCEPTION_IO);
+		}
+		return null;
+	}
+
+	/**
+	 * Creates the authorisation code flow needed for the authorisation URL.
+	 * 
+	 * @param httpTransport
+	 * @param jsonFactory
+	 * @param fdsf           FileDataStoreFactory
+	 * @return               GoogleAuthorizationCodeFlow object
+	 * @throws IOException
+	 */
+	private GoogleAuthorizationCodeFlow buildAuthorisationCodeFlow(
+			HttpTransport httpTransport, 
+			JsonFactory jsonFactory,
+			FileDataStoreFactory fdsf) throws IOException {
+		return new GoogleAuthorizationCodeFlow.Builder(
 				httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET, Arrays.asList(TasksScopes.TASKS))
-		.setAccessType("online")
-		.setApprovalPrompt("auto").build();
+		.setAccessType(FLOW_ACCESS_TYPE)
+		.setApprovalPrompt(FLOW_APPROVAL_PROMPT)
+		.setDataStoreFactory(fdsf).build();
+	}
 
+	/**
+	 * Creates the authorisation URL, asks the user to open the URL and sign in, then type in the
+	 * authorisation code from Google.
+	 * @param flow
+	 */
+	private void askUserForAuthorisationCode(GoogleAuthorizationCodeFlow flow) {
 		String url = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI).build();
 		System.out.println("Please open the following URL in your browser then type the authorization code:");
 		System.out.println("  " + url);
+	}
+
+	/**
+	 * Reads user input and returns it. 
+	 * @return      String of user input.
+	 */
+	private String getUserInput() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String code = "";
+		String input = "";
 		try {
-			code = br.readLine();
+			input = br.readLine();
 			br.close();
 		} catch (IOException e) {
 			System.out.println(MESSAGE_EXCEPTION_IO);
 		}
-
-		try {
-			GoogleTokenResponse response = flow.newTokenRequest(code).setRedirectUri(REDIRECT_URI).execute();
-			GoogleCredential credential = new GoogleCredential().setFromTokenResponse(response);
-			service = new Tasks.Builder(httpTransport, jsonFactory, credential).setApplicationName(APPLICATION_NAME).build();
-		} catch (IOException e) {
-			System.out.println(MESSAGE_EXCEPTION_IO);
-		}
+		return input;
 	}
 
 	/**
 	 * Prints out all tasks.
-	 * 
 	 * @return       Feedback for user.
 	 */
 	public String getAllTasks() {
